36e373a9189e6ea68a371eccf38377baebaf6410
Jingwen Owen Ou <jingweno@gmail.com>
2014-02-01 06:41:12 -0800

    Fix failling test on Travis

--- crawler/crawler_test.go
+++ crawler/crawler_test.go
@@ -9,10 +9,10 @@ import (
 
 func TestCrawler_buildColName(t *testing.T) {
 	form := "Jan 2, 2006 at 3:04pm (MST)"
-	tt, _ := time.Parse(form, "Feb 3, 2013 at 7:54pm (PST)")
+	tt, _ := time.Parse(form, "Feb 3, 2013 at 7:54pm (UTC)")
 	name := buildColName(&tt)
 
-	assert.Equal(t, name, "builds_2013_02_04")
+	assert.Equal(t, name, "builds_2013_02_03")
 }
 
 func TestCrawler_oneMinuteAgo(t *testing.T) {
