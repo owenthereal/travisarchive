a763b353690c59ffe9db19dd124a65e7e7d72744
Jingwen Owen Ou <jingweno@gmail.com>
2014-02-12 00:14:29 -0800

    Sort files by time

--- web/main.go
+++ web/main.go
@@ -4,12 +4,19 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
 
 	"github.com/codegangsta/martini"
 	"github.com/codegangsta/martini-contrib/render"
 	"github.com/jingweno/travisarchive/filestore"
 	"github.com/joho/godotenv"
 )
+
+type ByTime []filestore.File
+
+func (t ByTime) Len() int           { return len(t) }
+func (t ByTime) Swap(i, j int)      { t[i], t[j] = t[j], t[i] }
+func (t ByTime) Less(i, j int) bool { return t[i].Time.Before(t[j].Time) }
 
 func init() {
 	godotenv.Load("../.env")
@@ -31,6 +38,7 @@ func main() {
 	}))
 	m.Get("/", func(fs *filestore.S3, r render.Render) {
 		files, err := fs.List("builds")
+		sort.Sort(ByTime(files))
 		if err != nil {
 			log.Fatal(err)
 		}
